416c4048c625456f79d8f6735492ab42895d14f9
--add <jesusdmonteroc@gmail.com>
2020-03-29 22:16:28 -0500

    2

---
+++ reto_pentagono_puntos.js
@@ -0,0 +1,26 @@
+for (let i = 1; i < 10; i++) {
+
+    function pentagono(){
+      var pn = ((5*Math.pow(i,2))+(-5*i+2))/2
+      return pn
+  }
+    console.log('El pentagono ' + i + ' tiene ' + ' ' +pentagono() + ' puntos')
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
